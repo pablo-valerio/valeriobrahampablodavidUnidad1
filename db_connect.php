3917d4c0f391b691f0f0f67bbb3b4d31108680b0
pablo-valerio <pablosharyl0304@icloud.com>
2025-10-24 10:09:41 -0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,16 +3,11 @@
 
 $servername = "localhost";
 $username = "root"; // Usuario por defecto de XAMPP
-$password = ""; // Contraseña por defecto de XAMPP (suele estar vacía)
+$password = ""; // Contraseña por defecto de XAMPP 
 $dbname = "proyecto_login"; // El nombre de la DB que creaste
-
-// Crea la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica la conexión
 if ($conn->connect_error) {
-    // Para un entorno de producción, este mensaje debe ser menos detallado.
     die("Conexión fallida: " . $conn->connect_error);
 }
-// echo "Conexión exitosa"; // Descomenta solo para probar
 ?>
