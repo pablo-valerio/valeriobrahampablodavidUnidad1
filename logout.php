6825377efdf45fcd8d792d080caad90d3bd570bc
pablo-valerio <pablosharyl0304@icloud.com>
2025-10-24 10:10:59 -0600

    Clean up logout.php by removing comments and whitespace
    Removed commented code and unnecessary whitespace.

--- logout.php
+++ logout.php
@@ -1,12 +1,6 @@
 <?php
-// logout.php
-
 session_start();
-
-// Destruye todas las variables de sesión
 $_SESSION = array();
-
-// Si se usa `session_id()`, también se debe borrar la cookie de sesión.
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -14,11 +8,7 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
-
-// Finalmente, destruye la sesión.
 session_destroy();
-
-// Redirige al inicio de sesión
 header("Location: index.php");
 exit;
 ?>
